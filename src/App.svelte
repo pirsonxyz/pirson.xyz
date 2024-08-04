37eb57fe9397f07b2bf75cfd416f417c8053428f
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-08-04 14:57:26 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import myface from '/me.jpg';
+</script>
+
+<main>
+  <div>
+    <a href="https://github.com/pirsonxyz" target="_blank" rel="noreferrer">
+      <img src={myface} class="img" alt="Pfp" />
+    </a>
+    
+  </div>
+  <h1>Pirson Bethancourt</h1>
+  <p>
+   Software Developer from the Dominican Republic
+  </p>
+
+  <p class="presentation">
+    My links
+    </p>
+    <a href="https://github.com/pirsonxyz"><button>Github</button></a>
+    <a href="https://x.com/pirsonxyz"><button>X</button></a>
+    <a href="mailto:pirsonbethancourt@proton.me"><button>Email me</button></a>
+</main>
+
+<style>
+    .img {
+        border-radius: 50%;
+    }
+  .presentation{
+    color: #888;
+  }
+</style>
