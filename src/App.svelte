9f06e44f1b97e992b58f7540118ecd93c0535172
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-08-10 20:08:16 -0400

    xd

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,6 @@
   </p>
 
   <p class="presentation">Links</p>
-
   <a href="https://github.com/pirsonxyz"><button>Github</button></a>
   <a href="https://x.com/pirsonxyz"><button>X</button></a>
   <a href="mailto:pirson@pirson.xyz"><button>Mail</button></a>
