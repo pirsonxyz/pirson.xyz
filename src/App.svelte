851209d04e73e171ddcaba72eb200aed575fe902
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-08-29 23:04:20 -0400

    update pfp

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-  import myface from '/me.jpg';
-  import CV from '/PirsonBethancourt.pdf'
+  import myface from "/me.jpg";
+  import snorlax from "/snorlax.webp";
+  import CV from "/PirsonBethancourt.pdf";
 </script>
 
 <main>
   <div>
     <a href="https://github.com/pirsonxyz" target="_blank" rel="noreferrer">
-      <img src={myface} class="img" alt="Pfp" />
+      <img src={snorlax} class="img" alt="Pfp" />
     </a>
-    
   </div>
   <h1>Pirson Bethancourt</h1>
-  <p>
-   Software Developer from the Dominican Republic
-  </p>
+  <p>Software Developer from the Dominican Republic</p>
 
   <p class="presentation">Links</p>
 
@@ -22,14 +20,15 @@
   <a href="mailto:pirson@pirson.xyz"><button>Mail</button></a>
   <a href={CV}><button>CV</button></a>
   <a href="https://pirson.bearblog.dev"><button>Blog</button></a>
-
 </main>
 
 <style>
-    .img {
-        border-radius: 50%;
-    }
-  .presentation{
+  .img {
+    border-radius: 50%;
+    width: 769px;
+    height: auto;
+  }
+  .presentation {
     color: #888;
   }
 </style>
